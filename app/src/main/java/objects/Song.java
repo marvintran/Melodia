c8db026a829d3aee2a94f95b31040937d88d296e
Ben Mah <mahb@myumanitoba.ca>
2019-02-04 21:00:57 -0600

    Created methods for the Song object

--- app/src/main/java/objects/Song.java
+++ app/src/main/java/objects/Song.java
@@ -1,4 +1,23 @@
 package objects;
 
 public class Song {
+    private String songName;
+    private String artistName;
+    private int songTime;
+
+    public Song(String newSongName, String newArtistName, int newSongTime){
+        this.songName = newSongName;
+        this.artistName = newArtistName;
+        this.songTime = newSongTime
+    }
+
+    public String getSongName() { return songName; }
+
+    public String getArtistName() {return artistName; }
+
+    public int getSongTime() { return songTime; }
+
+    public String toString(){
+        return String(songName + " by: " + artistName + " is " + )
+    }
 }
