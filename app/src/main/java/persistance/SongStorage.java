4abc8e96fa38d50c54796dd05f19b097efcfab32
Ben Mah <mahb@myumanitoba.ca>
2019-02-04 20:51:17 -0600

    Changed package names to lowercase

--- app/src/main/java/persistance/SongStorage.java
+++ app/src/main/java/persistance/SongStorage.java
@@ -1,4 +1,4 @@
-package Persistance;
+package persistance;
 
 public class SongStorage {
 }
---
+++ app/src/main/java/presentation/SongActivity.java
@@ -0,0 +1,8 @@
+package presentation;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SongActivity extends AppCompatActivity {
+
+
+}
