76acace66f4482590ce53ef36c2514fd2c3e78c4
Su Dong <dongs@myumanitoba.ca>
2019-02-06 09:44:26 -0600

    Upload New File

---
+++ app/src/main/java/comp3350/melodia/objects/Account.java
@@ -0,0 +1,103 @@
+package com.example.melodia;
+
+public class Account{
+	
+	private String fullName;
+	private String userName;
+	private String email;
+	private Payment payInfo;
+	private String profile;
+	private String[] favoriteList;
+	//private AccessAccount userAccess;
+	
+	public Account(String fullName, String userName, String email, String profile, String[] favoriteList){
+		this.fullName = fullName;
+		this.userName = userName;
+		this.email = email;
+		this.profile = profile;
+		this.favoriteList = favoriteList;
+		//userAccess = null;
+	}
+	
+	public String getFullName(){
+		return fullName;
+	}
+	
+	public void setFullName(String inputName){
+		this.fullName = inputName;
+	}
+	
+	public String getProfile(){
+		return profile;
+	}
+	
+	public void setProfile(String profile){
+		this.profile = profile;
+	}
+	
+	public void setUserName(String newName){
+		this.userName = newName;
+	}
+	
+	public String getUserName(){
+		return userName;
+	}
+	
+	public void setEmail(String inputEmail){
+		this.email = inputEmail;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setFavorList(String[] newList){
+		this.favoriteList = newList;
+	}
+	
+	public String[] getFavorList(){
+		return favoriteList;
+	}
+	
+	public void printFavorList(){
+		System.out.println("Favourite songs list of :");
+		String result = "";
+		for(int i = 0; i < favoriteList.length; i++){
+				result += favoriteList[i] + "\n";
+		}
+		System.out.println(result);
+	}
+	
+	//public void setAccess(String userID, String password){
+		//this.userAccess = new AccessAccount(userID, password);
+	//}use for test
+	
+	//public AccessAccount getUserAccess(){
+		//return this.userAccess;
+	//}//use for test
+	
+	public String toString(){
+		String result = "";
+		result += "Name :" + fullName+" \n";
+		result += "UserName: " + userName + " \n";
+		result += "eMail : " + email + " \n";
+		result += "Profile: " + profile;
+		return result;
+	}
+	
+}
+class Payment{
+	private int bankAccount;
+	private String accountHolder;
+	private int vaildDate;
+	private Payment secondPayment;
+	
+	Payment(int bankAccount, String accountHolder, int vaildDate, Payment secondPayment){
+		this.bankAccount = bankAccount;
+		this.accountHolder = accountHolder;
+		this.vaildDate = vaildDate;
+		secondPayment = null;
+	}
+	
+	
+}
