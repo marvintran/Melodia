6c559d96525dd5d3a192b9e068be42cd0b6efe1a
Ben Mah <mahb@myumanitoba.ca>
2019-02-10 02:53:19 -0600

    Text properly updates

--- app/src/main/java/comp3350/melodia/presentation/SongActivity.java
+++ app/src/main/java/comp3350/melodia/presentation/SongActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,28 +16,41 @@ import comp3350.melodia.objects.Song;
 
 public class SongActivity extends Activity {
 
-    List<Song> SongList;
-    Song currSong;
-    AccessSong accessSong;
+    private List<Song> songList;
+    private int currSong;
+    private AccessSong accessSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
         accessSong = new AccessSong();
-        SongList = accessSong.getSongs();
+
+        try{
+            songList = accessSong.getSongs();
+            currSong = 0;
+
+            Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+            buttonPlay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView t1 = findViewById(R.id.textSong);
+                    t1.setText(songList.get(currSong).getSongName());
+
+                    TextView t2 = findViewById(R.id.textArtist);
+                    t2.setText(songList.get(currSong).getArtist().getArtistName());
+
+                    TextView t3 = findViewById(R.id.textSongTime);
+
+                    t3.setText(songList.get(currSong).getSongTimeString());
+                }
+            });
+        } catch(Exception e){
+            System.out.println("Error:" + e);
+        }
+
     }
 
-    protected void buttonPlayOnClick(){
-        TextView t1 = findViewById(R.id.textSong);
-        t1.setText(currSong.getSongName());
-
-        TextView t2 = findViewById(R.id.textSongTime);
-        t2.setText(currSong.getSongTime());
-
-        TextView t3 = findViewById(R.id.textArtist);
-        t3.setText(currSong.getArtist().getArtistName());
-    }
 
     public void buttonMenuOnClick(View v){
         Intent songIntent = new Intent( SongActivity.this, HomeActivity.class);
