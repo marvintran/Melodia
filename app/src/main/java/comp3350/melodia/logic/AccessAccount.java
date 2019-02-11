d634daca41163cc359ef4409a1444fa45f121738
Su Dong <dongs@myumanitoba.ca>
2019-02-10 20:43:36 -0600

    Replace AccessAccount.java

--- app/src/main/java/comp3350/melodia/logic/AccessAccount.java
+++ app/src/main/java/comp3350/melodia/logic/AccessAccount.java
@@ -1,95 +1,32 @@
-package com.example.melodia;
+package comp3350.melodia.logic;
+
+import java.util.Collection;
+import java.util.List;
+
+import comp3350.melodia.application.Services;
+import comp3350.melodia.objects.Account;
+import comp3350.melodia.persistance.AccountPersistence;
 
 public class AccessAccount{
-
-	private String loginID;
-	private String password;
-	private String[] securityQues;
-	private String[] securityAws;
-	private int numOfQuestion;
-	private boolean secondPwSetted;
-	private String secondPassword;
+	private AccountPersistence accountPersistence;
+	private List<Account> accountList;
+	private Account account;
 	
-	public AccessAccount(String loginID, String password){
-		this.password = password;
-		this.loginID = loginID;
-		this.secondPassword = password;
-		numOfQuestion = 0;
-		secondPwSetted = false;
-		securityQues = new String[3];
-		securityAws = new String[3];
+	public AccessAccount(){
+		accountPersistence = Services.getAccountPersistence();
+		accountList = null;
+		account = null;
+	}
+	
+	public list<Account> getAccount(){
+		
+		accountList = accountPersistence.getAllAccount();
+		
+		return Collections.unmodifiableList(accountList);
 		
 	}
 	
-	public void setSecurityQuestion(String question, String answer){
-		if(numOfQuestion >2 ){
-			System.out.println("Three Secrity Question Setted, Cannot add more");
-		}
-		else{
-			securityQues[numOfQuestion] = question;
-			securityAws[numOfQuestion] = answer;
-			numOfQuestion ++;
-		}
+	public Account insertAccount(Account newAccount){
+		return accountPersistence.insertAccount(newAccount);
 	}
-	
-	public boolean checkSecurityQuestion(String question, String answer){
-		boolean result = false;
-		for(int i = 0; i < 3; i++){
-			if(securityQues[i] == question){
-				if(securityAws[i].compareTo(answer) == 0){
-					result = true;
-				}
-			}
-		}
-		return result;
-	}
-	
-	public void setSecondPassword(String inputPassword){
-		this.secondPassword = inputPassword;
-		secondPwSetted = true;
-	}
-	
-	public boolean checkPassword(String inputPassword){
-		boolean result = false;
-		if(inputPassword.compareTo(password) == 0){
-			result = true;
-		}
-		
-		return result;
-	}
-	
-	
-	public boolean changePassword(int opition, String newPassword, String ip1, String ip2){
-		boolean result = false;
-		if(opition == 1){//user use secrity question to change password
-			if(checkSecurityQuestion(ip1,ip2)){
-				this.password = newPassword;
-				result = true;
-			}
-		}
-		else if(opition == 2){//user use second password to change password
-			if(ip1.compareTo(secondPassword) == 0){
-				this.password = newPassword;
-				result = true;
-			}
-		}
-		return result;
-	
-	}
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
