02c665781e22eaa40448536dc217e1245c36e7cc
tranm346@myumanitoba.ca <tranm346@myumanitoba.ca>
2019-02-24 03:25:05 -0600

    Implemented click and long click for a song in the library

--- app/src/main/java/comp3350/melodia/presentation/HomeFragment.java
+++ app/src/main/java/comp3350/melodia/presentation/HomeFragment.java
@@ -7,25 +7,26 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.melodia.R;
 import comp3350.melodia.logic.AccessSong;
-import comp3350.melodia.objects.Album;
-import comp3350.melodia.objects.Artist;
 import comp3350.melodia.objects.Song;
 
-// this is the song library screen for now
-public class HomeFragment extends Fragment{
+// this is the song library screen
+public class HomeFragment extends Fragment implements LibraryRecyclerViewAdapter.OnSongClickedListener,
+                                                      LibraryRecyclerViewAdapter.OnSongLongClickedListener {
 
     private List<Song> songList;
     private AccessSong accessSong;
 
-    RecyclerView myRecyclerView;
+    private RecyclerView myRecyclerView;
     private RecyclerView.Adapter myAdapter;
     private RecyclerView.LayoutManager myLinearLayout;
+
+    private Toast toastMessage;
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -54,7 +55,40 @@ public class HomeFragment extends Fragment{
         myRecyclerView.setLayoutManager(myLinearLayout);
 
         // define the adapter that will communicate between the dataset and the RecycleView
-        myAdapter = new LibraryRecyclerViewAdapter(songList);
+        myAdapter = new LibraryRecyclerViewAdapter(songList, this, this);
         myRecyclerView.setAdapter(myAdapter);
+    }
+
+    // passing data from Adapter to Fragment
+    // https://developer.android.com/guide/components/fragments.html#EventCallbacks
+    // https://stackoverflow.com/a/52830847
+
+    // when we click on a song in the library, it queues the song and starts playing it
+    public void onSongClicked(Song theSong)
+    {
+        // tell user the song clicked was added to the queue
+        String songTitle = theSong.getSongName();
+
+        // cancel the current toast message if there is one currently displaying
+        // this way we don't have to wait for the previous message to fully complete it's animation
+        if(toastMessage != null)
+            toastMessage.cancel();
+
+        toastMessage = Toast.makeText(getActivity(), "Added \"" + songTitle + "\" to the queue", Toast.LENGTH_SHORT);;
+        toastMessage.show();
+
+        // add this song to the current queue and play it
+    }
+
+    // long clicking a song in the library should open a context menu with various options
+    public void onSongLongClicked(Song theSong)
+    {
+        toastMessage = Toast.makeText(getActivity(), "Long Clicked: Open Context Menu", Toast.LENGTH_SHORT);
+        toastMessage.show();
+
+        // open context menu,
+
+        // option to add this song to the currently playing queue
+        // option to add this song to a playlist
     }
 }
--- app/src/main/java/comp3350/melodia/presentation/LibraryRecyclerViewAdapter.java
+++ app/src/main/java/comp3350/melodia/presentation/LibraryRecyclerViewAdapter.java
@@ -4,9 +4,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.melodia.R;
@@ -21,25 +21,41 @@ import comp3350.melodia.objects.Song;
 // https://guides.codepath.com/android/using-the-recyclerview
 public class LibraryRecyclerViewAdapter extends RecyclerView.Adapter<LibraryRecyclerViewAdapter.SongViewHolder>{
 
-    List<Song> songs;
+    private List<Song> songs;
+    private OnSongClickedListener listenerClick;
+    private OnSongLongClickedListener listenerLongClick;
+
+    // HomeFragment must implement these interfaces
+    // so that the adapter can send data back to the fragment
+    public interface OnSongClickedListener {
+        public void onSongClicked(Song theSong);
+    }
+    public interface OnSongLongClickedListener {
+        public void onSongLongClicked(Song theSong);
+    }
 
     // creates a SongViewHolder which contains references to all the views in this view row
     public static class SongViewHolder extends RecyclerView.ViewHolder {
         TextView songName;
         TextView artistName;
         TextView trackDuration;
+        LinearLayout linearlayout;
 
         SongViewHolder(View itemView) {
             super(itemView);
             songName = (TextView)itemView.findViewById(R.id.song_name);
             artistName = (TextView)itemView.findViewById(R.id.artist_name);
             trackDuration = (TextView)itemView.findViewById(R.id.track_duration);
+            this.linearlayout = (LinearLayout) itemView.findViewById(R.id.linearlayout);
         }
     }
 
     // constructor
-    public LibraryRecyclerViewAdapter(List<Song> songs){
+    public LibraryRecyclerViewAdapter(List<Song> songs, OnSongClickedListener listenerClick,
+                                                        OnSongLongClickedListener listenerLongClick){
         this.songs = songs;
+        this.listenerClick = listenerClick;
+        this.listenerLongClick = listenerLongClick;
     }
 
     // RecyclerView.Adapter has three abstract methods that we must override
@@ -52,7 +68,7 @@ public class LibraryRecyclerViewAdapter extends RecyclerView.Adapter<LibraryRecy
         return songs.size();
     }
 
-    // creates a view row for an library_item where views contained in the view row are stored in a SongViewHolder
+    // creates a view row for a library_item where views contained in the view row are stored in a SongViewHolder
     // does not give the views in the SongViewHolder any data,
     @Override
     public SongViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -65,9 +81,34 @@ public class LibraryRecyclerViewAdapter extends RecyclerView.Adapter<LibraryRecy
 
     // populates the views contained in this SongViewHolder
     @Override
-    public void onBindViewHolder(SongViewHolder songViewHolder, int viewType) {
+    public void onBindViewHolder(final SongViewHolder songViewHolder, final int viewType) {
         songViewHolder.songName.setText(songs.get(viewType).getSongName());
         songViewHolder.artistName.setText(songs.get(viewType).getArtist().getArtistName());
         songViewHolder.trackDuration.setText(songs.get(viewType).getSongTimeString());
+
+        // implementing onClick() in RecyclerView https://stackoverflow.com/a/38090900
+
+        // clicking on an item in the list
+        songViewHolder.linearlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if a song was clicked in the library, add it to the queue
+                // pass the song clicked to the fragment so it can add it to the queue
+                Song songClicked = songs.get(viewType);
+                listenerClick.onSongClicked(songClicked);
+            }
+        });
+
+        // long clicking on an item in the list
+        songViewHolder.linearlayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // if a song was long clicked in the library, open context menu
+                // pass the song to the fragment so we can do stuff depending on the menu option selected
+                Song songClicked = songs.get(viewType);
+                listenerLongClick.onSongLongClicked(songClicked);
+                return true;
+            }
+        });
     }
 }
