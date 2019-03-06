cd905c8a60ebac20496329365e416d0127b2976c
tranm346@myumanitoba.ca <tranm346@myumanitoba.ca>
2019-03-05 23:40:03 -0600

    Refactored the adapters that work with song lists by making an abstract class for reuse.

--- app/src/main/java/comp3350/melodia/presentation/PlaylistSongsRecyclerViewAdapter.java
+++ app/src/main/java/comp3350/melodia/presentation/PlaylistSongsRecyclerViewAdapter.java
@@ -1,28 +1,21 @@
 package comp3350.melodia.presentation;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.List;
 
 import comp3350.melodia.R;
 import comp3350.melodia.objects.Song;
 
-public class PlaylistSongsRecyclerViewAdapter extends LibraryRecyclerViewAdapter {
+public class PlaylistSongsRecyclerViewAdapter extends AbstractSongAdapter {
 
-    // constructor
     public PlaylistSongsRecyclerViewAdapter(List<Song> songs, OnSongClickedListener listenerClick,
                                             OnSongLongClickedListener listenerLongClick){
         super(songs, listenerClick, listenerLongClick);
     }
 
-    // creates a view row for a playlist_songs_item where views contained in the view row are stored in a SongViewHolder
-    // does not give the views in the SongViewHolder any data,
-    // the layout is the same as LibraryRecyclerViewAdapter except that it contains a dragHandle for reordering songs
     @Override
     public SongViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a view row as defined by our library_item.xml file
