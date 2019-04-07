d7f4c566dda63b5cd4d0224248be719ea03a116e
tranm346@myumanitoba.ca <tranm346@myumanitoba.ca>
2019-04-07 00:17:36 -0500

    Added simple unit test using mockito

--- app/src/test/java/com/example/melodia/business/AccessPlaylistTest.java
+++ app/src/test/java/com/example/melodia/business/AccessPlaylistTest.java
@@ -3,32 +3,46 @@ package com.example.melodia.business;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.melodia.logic.AccessPlaylist;
 import comp3350.melodia.objects.Playlist;
-import com.example.melodia.Persistence.PlaylistPersistenceStub;
+import comp3350.melodia.persistence.PlaylistPersistence;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class AccessPlaylistTest {
 
     private AccessPlaylist accessPlaylist;
+    private PlaylistPersistence playlistPersistence;
+
     @Before
-    public void setUp() {
-        accessPlaylist = new AccessPlaylist(new PlaylistPersistenceStub());
+    public void setup() {
+        playlistPersistence = mock(PlaylistPersistence.class);
+        accessPlaylist = new AccessPlaylist(playlistPersistence);
     }
 
     @Test
     public void test1()
     {
-        List<Playlist> playlists = accessPlaylist.getPlaylists();
+        final List<Playlist> playlists;
 
         System.out.println("\nStarting test AccessPlaylistTest");
+        List<Playlist> mockPlaylists = new ArrayList<>();
+        Playlist playlist1 = new Playlist(2, "Playlist 1", 0);
+        mockPlaylists.add(playlist1);
+        when(playlistPersistence.getAllPlaylists()).thenReturn((mockPlaylists));
 
+        playlists = accessPlaylist.getPlaylists();
         assertNotNull("The list of playlists should not be null", playlists);
         assertTrue(playlists.get(0).getPlaylistName().equals("Playlist 1"));
+
+        verify(playlistPersistence).getAllPlaylists();
 
         System.out.println("Finished test AccessPlaylist");
     }
--- app/src/test/java/com/example/melodia/business/AccessSongTest.java
+++ app/src/test/java/com/example/melodia/business/AccessSongTest.java
@@ -3,30 +3,55 @@ package com.example.melodia.business;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.melodia.logic.AccessSong;
 import comp3350.melodia.objects.Song;
-import com.example.melodia.Persistence.SongPersistenceStub;
+import comp3350.melodia.persistence.SongPersistence;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class AccessSongTest {
     private AccessSong accessSong;
+    private SongPersistence songPersistence;
 
     @Before
-    public void setUp(){
-        accessSong = new AccessSong(new SongPersistenceStub());
+    public void setup(){
+        songPersistence = mock(SongPersistence.class);
+        accessSong = new AccessSong(songPersistence);
     }
 
     @Test
     public void accessSongTest(){
-        List<Song> songs = accessSong.getSongsSortedTrackName();
+        final List<Song> songs;
+
         System.out.println("\nStarting test AccessSong");
+        List<Song> mockSongs = new ArrayList<>();
+        Song song1 = new Song.Builder()
+                .withSongID(1)
+                .withSongName("All that")
+                .withSongTime(177)
+                .withAlbumID(1)
+                .withAlbumName("bensound")
+                .withArtistID(1)
+                .withArtistName("Benjamin Tissot")
+                .withTrackNumber(3)
+                .withSongData("bensound_allthat.mp3")
+                .build();
+        mockSongs.add(song1);
+        when(songPersistence.getSongsSortedTrackName()).thenReturn((mockSongs));
+
+        songs = accessSong.getSongsSortedTrackName();
         assertNotNull("The list of songs should not be null", songs);
         assertEquals("All that", songs.get(0).getSongName());
+
+        verify(songPersistence).getSongsSortedTrackName();
+
         System.out.println("Finished test AccessSong");
     }
 
