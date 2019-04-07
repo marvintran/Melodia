e380d87073c2812dd7a01cb5276779637029e754
Ben Mah <mahb@myumanitoba.ca>
2019-04-06 23:55:24 -0500

    merge to benWorkspace

--- app/src/main/java/comp3350/melodia/presentation/SongFragment.java
+++ app/src/main/java/comp3350/melodia/presentation/SongFragment.java
@@ -52,8 +52,8 @@ public class SongFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState){
-        AccessSong accessSong = new AccessSong(Services.getSongPersistence());
-        songList = accessSong.getSongsSortedTrackName();
+        //AccessSong accessSong = new AccessSong(Services.getSongPersistence());
+        //songList = accessSong.getSongsSortedTrackName();
         currSong = 0;
         player = new MediaPlayer();
         seekbarHandler = new Handler();
@@ -65,6 +65,11 @@ public class SongFragment extends Fragment {
         newPlaylist.setOnClickListener(mButtonClickListener);
 
         return v;
+    }
+
+    public void updateSongList(){
+        AccessSong accessSong = new AccessSong(Services.getSongPersistence());
+        songList = accessSong.getPlaylistSongs(0);
     }
 
     @Override
