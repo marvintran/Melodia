29bce9ced4645f58132351437f0809baa136d4df
emc2jah <emc2jah@hotmail.com>
2019-04-08 05:41:05 -0500

    code cleanup

--- app/src/main/java/comp3350/melodia/presentation/AppCompatPreferenceActivity.java
+++ app/src/main/java/comp3350/melodia/presentation/AppCompatPreferenceActivity.java
@@ -12,10 +12,6 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
- * to be used with AppCompat.
- */
 public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
 
     private AppCompatDelegate mDelegate;
@@ -31,14 +27,6 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         getDelegate().onPostCreate(savedInstanceState);
-    }
-
-    public ActionBar getSupportActionBar() {
-        return getDelegate().getSupportActionBar();
-    }
-
-    public void setSupportActionBar(@Nullable Toolbar toolbar) {
-        getDelegate().setSupportActionBar(toolbar);
     }
 
     @Override
